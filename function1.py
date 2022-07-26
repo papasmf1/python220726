4d76bdb0f6b8ea70320d7a366f4faff19fd27094
papasmf1 <papasmf1@gmail.com>
2022-07-26 11:51:47 +0900

    교집합 문자를 리턴

--- function1.py
+++ function1.py
@@ -8,3 +8,23 @@ def setValue(newValue):
 retValue = setValue(5)
 print(retValue)
 
+#다중의 값을 리턴
+def swap(x,y):
+    return y,x 
+
+#호출
+result = swap(3,4)
+print(result[0])
+print(result[1])
+
+#교집합 문자를 리턴하는 함수
+def intersect(prelist, postlist):
+    #지역변수
+    result = []
+    for x in prelist:
+        if x in postlist and x not in result:
+            result.append(x)
+    return result 
+
+#함수 호출
+print(intersect("HAM","SPAM"))
