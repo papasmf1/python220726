9d0a8f9e880924bcda6f43d10618ba3a380dd593
papasmf1 <papasmf1@gmail.com>
2022-07-26 14:44:41 +0900

    함수 연습 5

--- function3.py
+++ function3.py
@@ -17,3 +17,26 @@ def connectURI(server, port):
 print(connectURI("credu.com", "80"))
 print(connectURI(port="80", server="credu.com"))
 
+#가변인자
+def union(*ar):
+    result = []
+    for item in ar:
+        for x in item:
+            if x not in result:
+                result.append(x)
+    return result 
+
+#호출 
+print(union("HAM","EGG"))
+print(union("HAM","EGG","SPAM"))
+
+#정의되지 않은 인자(필수와 옵션이 있는 경우)
+def userURIBuilder(server, port, **user):
+    strURL = "http://" + server + ":" + port + "/?"
+    for key in user.keys():
+        strURL += key + "=" + user[key] + "&"
+    return strURL 
+#호출
+print(userURIBuilder("credu.com", "80", id="kim"))
+print(userURIBuilder("credu.com", "80", id="kim", passwd="1234"))
+
