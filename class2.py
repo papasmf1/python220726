99c770596408a27d7adf942f5701fb36e2411fb6
papasmf1 <papasmf1@gmail.com>
2022-07-26 16:35:08 +0900

    클래스 연습4

--- class2.py
+++ class2.py
@@ -5,7 +5,9 @@ class Person:
     num_person = 0 
     #초기화 메서드
     def __init__(self):
+        #소속이 인스턴스 멤버 변수
         self.name = "default name"
+        #소속이 클래스 멤버 변수
         Person.num_person += 1 
     #인스턴스 메서드
     def print(self):
@@ -15,6 +17,7 @@ class Person:
 #2)인스턴스 생성(중단점을 지정)
 p1 = Person() 
 p2 = Person()
+p3 = Person() 
 print("인스턴스 개수:{0}".format(Person.num_person))
 
 
