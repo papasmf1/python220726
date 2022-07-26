acfacba0e68517f2d2e6e4f84af62dc5ae1f6e6f
papasmf1 <papasmf1@gmail.com>
2022-07-26 10:48:36 +0900

    반복문 연습2

--- DemoLoop.py
+++ DemoLoop.py
@@ -7,3 +7,20 @@ while value > 0:
 
 print("---반복구문---")
 
+d = {"apple":100, "kiwi":200, "orange":300}
+for item in d.items():
+    print(item)
+
+for i in [2,3,4,5,6]:
+    #서식을 지정해서 변경하는 문자 
+    print("---{0}단---".format(i))
+    for j in [1,2,3,4,5,6,7,8,9]:
+        print("{0} * {1} = {2}".format(i, j, i*j))
+
+print("---break구문---")
+lst = [1,2,3,4,5,6,7,8,9,10]
+for i in lst:
+    if i > 5:
+        break
+    print("Item:{0}".format(i))
+
