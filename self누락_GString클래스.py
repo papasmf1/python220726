7e7e0d51c9fdc47e0778092eb67b06347c58608d
papasmf1 <papasmf1@gmail.com>
2022-07-27 09:36:24 +0900

    꼼꼼하게 코딩하기

--- self누락_GString클래스.py
+++ self누락_GString클래스.py
@@ -1,12 +1,17 @@
+#전역변수(이름충돌)
 str = "Not Class Member"
+
 class GString:
     def __init__(self):
+        #인스턴스 멤버 변수
         self.str = "" 
     def set(self, msg):
         self.str = msg
     def print(self):
-        print(str)
+        #꼼꼼하게 코딩(명시적 코딩)
+        print(self.str)
 
+#인스턴스(object), 객체 
 g = GString()
 g.set("First Message")
 g.print()
