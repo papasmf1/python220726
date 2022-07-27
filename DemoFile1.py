85bebf908d1d2f37f33d9fda7679a0a8cd44f1d3
papasmf1 <papasmf1@gmail.com>
2022-07-27 15:44:07 +0900

    파일 입출력3

--- DemoFile1.py
+++ DemoFile1.py
@@ -5,4 +5,15 @@ f = open("c:\\work\\demo.txt", "wt")
 print("file write", file=f)
 f.close()
 
+print("---정렬---")
+for x in range(1,6):
+    print(x,"*",x,"=",x*x)
+
+print("---오른쪽정렬---")
+for x in range(1,6):
+    print(x,"*",x,"=", str(x*x).rjust(3))
+
+print("---공백문자---")
+for x in range(1,6):
+    print(x,"*",x,"=", str(x*x).zfill(3))
 
