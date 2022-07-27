2aafff314e09c70b22e6e553bfe114c588997ffa
papasmf1 <papasmf1@gmail.com>
2022-07-27 09:31:12 +0900

    동적 형식 연습

--- class2.py
+++ class2.py
@@ -20,7 +20,15 @@ p2 = Person()
 p3 = Person() 
 print("인스턴스 개수:{0}".format(Person.num_person))
 
-
+#동적으로 변수를 추가
+Person.title = "new title"
+print(p1.title)
+print(p2.title)
+print(Person.title)
+#인스턴스에 추가
+p1.age = 30
+print(p1.age)
+#print(p2.age)
 
 
     
