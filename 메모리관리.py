23c70f5dfad0f1fc5d7dbb20c2c8d4800e0f31d1
papasmf1 <papasmf1@gmail.com>
2022-07-27 09:44:34 +0900

    메모리 관리

--- 메모리관리.py
+++ 메모리관리.py
@@ -1,12 +1,17 @@
 # 메모리관리.py 
-
 class MyClass:
+    #가장 먼저 실행되는 초기화 루틴(생성자) 
     def __init__(self, value):
         self.value = value 
         print("Instance is created! value=", value)
+    #인스턴스를 파괴할 때 가장 마지막에 실행(소멸자)
     def __del__(self):
         print("Instance is deleted!")
 
+#인스턴스 생성 
 d = MyClass(5)
+#인스턴스 메모리 삭제 
 del d 
+
+print("---전체 코드 실행 종료---")
 
