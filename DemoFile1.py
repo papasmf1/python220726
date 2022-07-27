9df1fd80b67236fce5428c6b618c731c75789eda
papasmf1 <papasmf1@gmail.com>
2022-07-27 15:51:35 +0900

    서식 문자 지정

--- DemoFile1.py
+++ DemoFile1.py
@@ -17,3 +17,11 @@ print("---공백문자---")
 for x in range(1,6):
     print(x,"*",x,"=", str(x*x).zfill(3))
 
+#서식문자를 지정
+print("{0:x}".format(10))
+print("{0:b}".format(10))
+print("{0:c}".format(65))
+print("{0:,}".format(15000000))
+print("{0:e}".format(4/3))
+print("{0:f}".format(4/3))
+print("{0:.2f}".format(4/3))
