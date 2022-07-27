2597bd11467c7cbf32e6c7ee30fe0234e02dfddf
papasmf1 <papasmf1@gmail.com>
2022-07-27 16:28:16 +0900

    파일 연습 5

--- DemoFile2.py
+++ DemoFile2.py
@@ -1,6 +1,19 @@
 # DemoFile2.py 
-#파일 쓰기
+#파일 쓰기(raw string notation)
 f = open(r"c:\work\demo.txt", "wt", encoding="utf-8")
 f.write("첫번째\n두번째\n세번째\n")
 f.close() 
+
+#파일을 읽기
+f = open("c:/work/demo.txt", "rt", encoding="utf-8")
+#print(f.read())
+#리스트에 담기 
+lst = f.readlines()
+print(lst)
+f.close()
+
+for item in lst:
+    print(item)
+
+
 
