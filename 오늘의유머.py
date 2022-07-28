b2bc89822835c9fc25c4bf3829ba37ebe9c33265
papasmf1 <papasmf1@gmail.com>
2022-07-28 14:42:28 +0900

    오늘의 유머 크롤링

--- 오늘의유머.py
+++ 오늘의유머.py
@@ -6,9 +6,9 @@ import re
 #User-Agent를 조작하는 경우(아이폰에서 사용하는 사파리 브라우져의 헤더) 
 hdr = {'User-agent':'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/603.1.23 (KHTML, like Gecko) Version/10.0 Mobile/14E5239e Safari/602.1'}
 
-for n in range(0,10):
-        #클리앙의 중고장터 주소 
-        data ='https://www.clien.net/service/board/sold?&od=T31&po=' + str(n)
+for n in range(1,11):
+        #오늘의유머(베스트게시판)
+        data ='http://www.todayhumor.co.kr/board/list.php?table=bestofbest&page=' + str(n)
         print(data)
         #웹브라우져 헤더 추가 
         req = urllib.request.Request(data, \
@@ -20,16 +20,16 @@ for n in range(0,10):
         soup = BeautifulSoup(page, 'html.parser')
         
         #검색 조건이 있는 태그(속성들): <span data-role= 
-        list = soup.find_all('span', attrs={'data-role':'list-title-text'})
-        # <span class="subject_fixed" data-role="list-title-text">
-        #   아이폰12 프로 256 골드 A급 풀박스 팝니다.
-	# </span>
+        list = soup.find_all('td', attrs={'class':'subject'})
+       
+        #<td class="subject">
+        # <a href="/board/view.php?table=bestofbest">고려장이 구라인 이유</a>
 
         for item in list:
                 try:
-                        title = item.text 
+                        title = item.find("a").text
                         #print(title.strip())
-                        if (re.search('맥북', title)):
+                        if (re.search('한국', title)):
                                 print(title.strip())
                 except:
                         pass
