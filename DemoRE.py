24a742f56c5e285c1d04d69b5b7d080b6fad25ea
papasmf1 <papasmf1@gmail.com>
2022-07-28 10:39:06 +0900

    로그 파일 분석

--- DemoRE.py
+++ DemoRE.py
@@ -1,12 +1,22 @@
 # DemoRE.py
 import re 
 
-result = re.search("[0-9]*th", "35th")
+#매칭오브젝트 
+result = re.search("[0-9]*th", "  35th")
 print(result)
 print(result.group())
 
-result = re.match("[0-9]*th", "35th")
-print(result)
+# result = re.match("[0-9]*th", "  35th")
+# print(result)
+# print(result.group())
+
+#search함수와 match함수의 차이
+print(bool(re.search("apple", "this is apple")))
+print(bool(re.match("apple", "this is apple")))
+print(bool(re.search("\d{4}", "올해는 2022년")))
+print(bool(re.search("\d{5}", "우리 동네는 52300")))
+
+result = re.search("\d{5}", "우리 동네는 52300")
 print(result.group())
 
 
